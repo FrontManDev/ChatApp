287c95eb4a4ad3e3c171b58083e6dcc8a64f2796
FrontManDev <habdulkarim139@gmail.com>
2025-04-05 15:26:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -203,7 +203,7 @@ app.post("/register", async (req, res) => {
     const Token = jwt.sign({ id: newUser.id, email: newUser.email }, TokenKey, {
       expiresIn: Time,
     });
-    res.status(201).json({ message: "User registered successfully", user: newUser, Token });
+    res.status(201).json({ message: "User registered successfully", User: {id: User.id,firstName: User.firstName}, Token });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error" });
